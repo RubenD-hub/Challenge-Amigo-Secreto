3c404162a25761c7936794a98d33a5e142237a30
Ruben Sanchez Loperena <rubenlop98@gmail.com>
2025-01-23 22:21:05 -0600

    Capturar el valor del campo de entrada

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Declaración de Variables
+let amigos=[];
+
+
+// Declaración de Funciones 
+
+function agregarAmigo(){
+    let nombreAmigo= String(document.getElementById('amigo').value)
+    amigos.push(nombreAmigo);
+    console.log(amigos)
+}
