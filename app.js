b26b64042fa77369380ac69483be2cde07379336
Ruben Sanchez Loperena <rubenlop98@gmail.com>
2025-01-23 22:25:04 -0600

    Validar la entrada, caso de entrada vacía

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 // Declaración de Variables
-let amigos=[];
+let amigos = [];
 
+// Declaración de Funciones
 
-// Declaración de Funciones 
-
-function agregarAmigo(){
-    let nombreAmigo= String(document.getElementById('amigo').value)
+function agregarAmigo() {
+  let nombreAmigo = String(document.getElementById("amigo").value);
+  if (nombreAmigo == "") {
+    alert("Por favor, inserte un nombre.");
+  } else {
     amigos.push(nombreAmigo);
-    console.log(amigos)
+    console.log(amigos);
+  }
 }
